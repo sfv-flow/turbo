e1739187546649751d924eff1eb5bca6a483a35f
itsnotaka <itsnotaka@gmail.com>
2022-11-17 18:18:25 +0800

    [ci] improved google auth and added test page

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -7,17 +7,24 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 export const authOptions: NextAuthOptions = {
 	// Configure one or more authentication providers
 	adapter: PrismaAdapter(prisma),
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID!,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-		}),
-		// ...add more providers here
-	],
 	callbacks: {
 		session({ session, user }) {
 			session.user.id = user.id;
 			return session;
 		},
 	},
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code",
+				},
+			},
+		}),
+		// ...add more providers here
+	],
 };
