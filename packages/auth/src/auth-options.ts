b5b5835644d278f3fe8d6b3aa8acbf21b69a4ef3
itsnotaka <itsnotaka@gmail.com>
2022-11-17 17:31:23 +0800

    [ci] added google login

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,5 +1,5 @@
 import { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { prisma } from "@flow/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -8,9 +8,9 @@ export const authOptions: NextAuthOptions = {
 	// Configure one or more authentication providers
 	adapter: PrismaAdapter(prisma),
 	providers: [
-		DiscordProvider({
-			clientId: process.env.DISCORD_CLIENT_ID!,
-			clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
 		}),
 		// ...add more providers here
 	],
