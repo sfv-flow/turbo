31e64e28afdd4fc03bc3ffcd4c594be88504bef3
itsnotaka <itsnotaka@gmail.com>
2022-11-19 15:41:32 +0800

    [ci] updated wrongfully import

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -7,16 +7,16 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    // Enables hot-reload and easy integration for local packages
-    transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
-  },
-  // We already do linting on GH actions
-  eslint: {
-    ignoreDuringBuilds: !!process.env.CI,
-  },
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		// Enables hot-reload and easy integration for local packages
+		transpilePackages: ["@flow/api", "@flow/auth", "@flow/db"],
+	},
+	// We already do linting on GH actions
+	eslint: {
+		ignoreDuringBuilds: !!process.env.CI,
+	},
 };
 
 export default config;
