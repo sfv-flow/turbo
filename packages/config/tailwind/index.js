254fb6fe3146b013e98b806910f1d9a2b8f22356
itsnotaka <itsnotaka@gmail.com>
2022-11-21 13:13:43 +0800

    [fix] tailwind extend fix

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}", "./src/_app.tsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{ts,tsx}", "./src/_app.tsx"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
