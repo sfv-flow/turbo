09fd7feba43936dafb875ae3245e06229832dd3b
itsnotaka <itsnotaka@gmail.com>
2022-11-23 16:58:34 +0800

    [format] prettier

--- apps/nextjs/src/components/CommandMenu.tsx
+++ apps/nextjs/src/components/CommandMenu.tsx
@@ -20,13 +20,13 @@ const CommandMenu = ({ label }: { label: string }) => {
 	return (
 		<Command.Dialog
 			className={clsx(
-				"max-w-[640px] w-full p-2 rounded-xl overflow-hidden border border-[#e2e2e2] shadow-[0_16px_70px_rgb(0_0_0_/_20%)] transition-transform duration-100 ease-in-out",
+				"w-full max-w-[640px] overflow-hidden rounded-xl border border-[#e2e2e2] p-2 shadow-[0_16px_70px_rgb(0_0_0_/_20%)] transition-transform duration-100 ease-in-out",
 			)}
 			open={open}
 			onOpenChange={setOpen}
 			label={label}
 		>
-			<Command.Input className="border-none w-full text-[17px] px-2 pt-2 pb-4 outline-none bg-[#e2e2e2] border-b-[1px_solid_hsl(0,_0%,_88.7%)] mb-4 rounded-none placeholder:text-[#8f8f8f]" />
+			<Command.Input className="mb-4 w-full rounded-none border-none border-b-[1px_solid_hsl(0,_0%,_88.7%)] bg-[#e2e2e2] px-2 pt-2 pb-4 text-[17px] outline-none placeholder:text-[#8f8f8f]" />
 			{/*  height: min(330px, calc(var(--cmdk-list-height)));
     max-height: 400px;
     overflow: auto;
@@ -38,14 +38,14 @@ const CommandMenu = ({ label }: { label: string }) => {
 				<Command.Empty>No results found.</Command.Empty>
 
 				<Command.Group heading="Letters">
-					<Command.Item className="[content-visibility:auto] cursor-pointer h-12 rounded-lg flex items-center gap-2 px-4 select-none will-change-[background,_color] transition-all transition-none aria-selected:bg-[#0000000d] aria-selected:text-[#171717]">
+					<Command.Item className="flex h-12 cursor-pointer select-none items-center gap-2 rounded-lg px-4 transition-all transition-none will-change-[background,_color] [content-visibility:auto] aria-selected:bg-[#0000000d] aria-selected:text-[#171717]">
 						a
 					</Command.Item>
-					<Command.Item className="[content-visibility:auto] cursor-pointer h-12 rounded-lg flex items-center gap-2 px-4 select-none will-change-[background,_color] transition-all transition-none">
+					<Command.Item className="flex h-12 cursor-pointer select-none items-center gap-2 rounded-lg px-4 transition-all transition-none will-change-[background,_color] [content-visibility:auto]">
 						a
 					</Command.Item>
 					<Command.Separator />
-					<Command.Item className="[content-visibility:auto] cursor-pointer h-12 rounded-lg flex items-center gap-2 px-4 select-none will-change-[background,_color] transition-all transition-none">
+					<Command.Item className="flex h-12 cursor-pointer select-none items-center gap-2 rounded-lg px-4 transition-all transition-none will-change-[background,_color] [content-visibility:auto]">
 						a
 					</Command.Item>
 				</Command.Group>
