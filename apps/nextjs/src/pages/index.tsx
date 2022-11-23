12714c89ce7a87476a0c85b6c0e0ed77ab10ff23
itsnotaka <itsnotaka@gmail.com>
2022-11-23 18:21:38 +0800

    [ci] added create workspace button (not working)

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -63,12 +63,12 @@ export const CreateWorkSpaceComponent = () => {
 							<div className="mb-6">
 								<div className="text-[#eeeffc]">
 									<label className="mb-1 block pl-[2px]">
-										<span className="text-left text-[0.8125rem] font-medium">
+										<span className="text-left text-[13px] font-medium">
 											Workspace Name
 										</span>
 									</label>
 									<input
-										className="m-0 h-12 w-full rounded-[4px] border border-[#393a4b] bg-[#151621] p-3 text-[0.8125rem] outline-none transition-[border] duration-150 focus:border-[#6C79FF] focus:shadow-none"
+										className="m-0 h-12 w-full rounded border border-[#393a4b] bg-[#151621] p-3 text-[13px] outline-none transition-[border] duration-150 focus:border-[#6C79FF] focus:shadow-none"
 										{...register("workspaceName", { required: true })}
 									/>
 								</div>
@@ -76,7 +76,7 @@ export const CreateWorkSpaceComponent = () => {
 							<div className="mb-6 text-[#eeeffc]">
 								{/* Workspace url field */}
 								<label className="mb-1 block pl-[2px]">
-									<span className="text-left text-[0.8125rem] font-medium">
+									<span className="text-left text-[13px] font-medium">
 										Workspace URL
 									</span>
 								</label>
@@ -91,7 +91,7 @@ export const CreateWorkSpaceComponent = () => {
 										</div>
 										<input
 											autoComplete="off"
-											className="m-0 h-12 w-full appearance-none rounded-[4px] border border-[#393a4b] bg-[#151621] p-3 pl-[84.5547px] text-[13px] font-medium outline-none transition-[border] duration-150 focus:border-[#6C79FF] focus:shadow-none"
+											className="m-0 h-12 w-full appearance-none rounded border border-[#393a4b] bg-[#151621] p-3 pl-[84.5547px] text-[13px] font-medium outline-none transition-[border] duration-150 focus:border-[#6C79FF] focus:shadow-none"
 											{...register("workspaceSlug", { required: true })}
 										/>
 									</div>
@@ -99,6 +99,19 @@ export const CreateWorkSpaceComponent = () => {
 							</div>
 						</form>
 					</div>
+				</div>
+				{/* submit button */}
+				<div>
+					<button
+						className="relative mt-6 inline-flex h-12 w-[340px] min-w-[32px] max-w-[90vw] flex-shrink-0 select-none items-center justify-center whitespace-nowrap rounded border border-[#575ac7] bg-[#575bc7] py-0 px-6 text-[13px] text-white [shadow:rgb(0_0_0/15%)_0px_1px_2px]"
+						draggable="false"
+						style={{
+							WebkitBoxAlign: "center",
+							WebkitBoxPack: "center",
+						}}
+					>
+						Create Workspace
+					</button>
 				</div>
 			</div>
 			{/* User Info */}
@@ -108,7 +121,7 @@ export const CreateWorkSpaceComponent = () => {
 			>
 				<button
 					draggable="false"
-					className="over:bg-[#272832] m-0 inline select-none overflow-hidden whitespace-nowrap rounded-[4px] border-0 bg-transparent p-2 text-left text-[#eeeffc] transition-colors duration-150 hover:bg-[#272832] hover:text-[#EEEFFC] hover:duration-[0]"
+					className="over:bg-[#272832] m-0 inline select-none overflow-hidden whitespace-nowrap rounded border-0 bg-transparent p-2 text-left text-[#eeeffc] transition-colors duration-150 hover:bg-[#272832] hover:text-[#EEEFFC] hover:duration-[0]"
 				>
 					<div className="flex flex-initial flex-col">
 						<span className="whitespace-nowrap text-left text-xs text-[#858699] duration-150 [transition-property:color,fill]">
@@ -129,7 +142,7 @@ export const CreateWorkSpaceComponent = () => {
 					onClick={() => {
 						signOut();
 					}}
-					className="m-0 inline-flex h-8 min-w-[32px] flex-shrink-0 select-none items-center justify-center whitespace-nowrap rounded-[4px] border border-transparent bg-transparent py-0 px-3 text-xs font-medium text-[#d2d3e0] shadow-none outline-none duration-150 [transition-property:border,background-color,color,box-shadow,opacity] hover:bg-[#272832] hover:text-[#EEEFFC] hover:duration-[0]"
+					className="m-0 inline-flex h-8 min-w-[32px] flex-shrink-0 select-none items-center justify-center whitespace-nowrap rounded border border-transparent bg-transparent py-0 px-3 text-xs font-medium text-[#d2d3e0] shadow-none outline-none duration-150 [transition-property:border,background-color,color,box-shadow,opacity] hover:bg-[#272832] hover:text-[#EEEFFC] hover:duration-[0]"
 					draggable="false"
 					style={{ WebkitBoxAlign: "center", WebkitBoxPack: "center" }}
 				>
