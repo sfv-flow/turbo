14d6734a6f5cd3351608bcb9dd02663f7c2299f1
itsnotaka <itsnotaka@gmail.com>
2022-11-23 11:57:50 +0800

    [refactor] refactored button to components

---
+++ apps/nextjs/src/components/buttons/SignInButton.tsx
@@ -0,0 +1,25 @@
+import { BuiltInProviderType } from "next-auth/providers";
+import { signIn } from "next-auth/react";
+import { GoogleIcon } from "../icons";
+
+const SignInButton = ({ provider }: { provider: BuiltInProviderType }) => {
+	const SignInHandler = (provider: BuiltInProviderType) => {
+		signIn(provider, { callbackUrl: "/" });
+	};
+	return (
+		<button
+			onClick={() => SignInHandler(provider)}
+			className=" m-0 inline-flex h-12 w-[336px] min-w-[32px] flex-shrink-0 select-none items-center justify-center whitespace-nowrap rounded-[4px] border border-[#6e79d6] bg-[#575bc7] py-0 px-[14px] font-medium text-white shadow-[rgb(0_0_0_/_7%)_0px_1px_2px] [transition-property:border,background-color,color,box-shadow,opacity]"
+			draggable="false"
+			style={{
+				WebkitBoxAlign: "center",
+				WebkitBoxPack: "center",
+			}}
+		>
+			<GoogleIcon className="mr-3 w-[14px] flex-shrink-0 text-white" />
+			Login with Google
+		</button>
+	);
+};
+
+export default SignInButton;
