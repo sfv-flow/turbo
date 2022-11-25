3480e955db668fada62aa155bf24c2ada61ac60d
itsnotaka <itsnotaka@gmail.com>
2022-11-25 12:13:15 -0500

    [ci] full rewrite on worksapce page logic

---
+++ apps/nextjs/src/pages/[slug]/inbox.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import { ReactElement } from "react";
+import Container from "../../components/Container";
+
+const Inbox = () => {
+	const router = useRouter();
+
+	// with require being true the user will be directly pushed to the login page if they are not logged in
+	// const { data: session } = useSession({
+	// 	required: true,
+	// });
+
+	return (
+		<p>This would be the Inbox content Current route: {router.pathname}</p>
+	);
+};
+
+export default Inbox;
+
+Inbox.getLayout = function getLayout(page: ReactElement) {
+	return <Container>{page}</Container>;
+};
