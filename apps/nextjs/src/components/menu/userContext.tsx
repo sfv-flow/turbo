91cc5e9f25ec96e5e11fc35c8634120fa9ea369e
itsnotaka <itsnotaka@gmail.com>
2022-11-26 12:35:40 -0500

    [ci] added user menu on icon click

---
+++ apps/nextjs/src/components/menu/userContext.tsx
@@ -0,0 +1,64 @@
+import { useRouter } from "next/router";
+import UserContextItem from "./userContextItem";
+
+const UserContextMenu = () => {
+	const router = useRouter();
+	return (
+		<div className="fixed z-[600] m-0 [inset:0px_auto_auto_0px] [transform:translate3d(-17px,4px,0px)]">
+			<div className="pointer-events-auto rounded-lg border-[0.5px] border-[#dfe1e4] bg-white/50 opacity-[2] shadow-[rgb(0_0_0/9%)_0px_3px_12px] [transform:scale(1)] [transform-origin:0px_0px] [backdrop-filter:blur(12px)_saturate(190%)_contrast(50%)_brightness(130%)]">
+				<div
+					className="m-0 flex min-w-min flex-shrink flex-grow flex-col overflow-hidden py-1 px-0 text-[13px]"
+					style={{
+						WebkitBoxFlex: 1,
+					}}
+				>
+					<div className="relative h-[278px] w-[186px] overflow-auto will-change-transform [direction:ltr]">
+						<div className="h-[278px] w-full flex-col">
+							<div className="absolute top-0 h-8 w-auto min-w-full">
+								<UserContextItem
+									title="View Profile"
+									href={{
+										pathname:
+											router.pathname === "/[slug]/profile"
+												? ""
+												: "/[slug]/profile",
+										query: { slug: router.query.slug },
+									}}
+								/>
+							</div>
+							<div className="absolute top-8 h-8 w-auto min-w-full">
+								<UserContextItem
+									title="Settings"
+									href={{
+										pathname:
+											router.pathname === "/[slug]/settings"
+												? ""
+												: "/[slug]/settings",
+										query: { slug: router.query.slug },
+									}}
+								/>
+							</div>
+							<div className="absolute top-16 h-8 w-auto min-w-full">
+								<div className="py-[5px] px-0 ">
+									<div className="[border-bottom:0.5px_solid_rgba(0,0,0,0.098)]" />
+								</div>
+								<UserContextItem
+									title="Contact support"
+									href={{
+										pathname:
+											router.pathname === "/[slug]/contact"
+												? ""
+												: "/[slug]/contact",
+										query: { slug: router.query.slug },
+									}}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default UserContextMenu;
---
+++ apps/nextjs/src/components/menu/userContextItem.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+const UserContextItem = ({
+	title,
+	href,
+}: {
+	title: string;
+	href: { pathname: string; query: { slug: string | string[] | undefined } };
+}) => {
+	return (
+		<Link
+			href={href}
+			className="relative flex h-8 w-full min-w-min max-w-[186px] items-center text-ellipsis whitespace-nowrap bg-transparent text-[#3c4149] [flex:0_0_100%] 
+            "
+			style={{
+				WebkitBoxFlex: 1,
+				WebkitBoxAlign: "center",
+			}}
+		>
+			<div
+				className="mx-1 flex h-full flex-initial flex-grow flex-row items-center overflow-hidden rounded-[6px] py-0 px-[10px] hover:bg-[#EBECEC]"
+				style={{
+					WebkitBoxFlex: 1,
+					WebkitBoxAlign: "center",
+				}}
+			>
+				<span className="inline-flex flex-grow items-center overflow-hidden text-ellipsis whitespace-nowrap text-left font-medium leading-normal text-[#282a30] ">
+					<span
+						className="mr-2 flex-shrink-[0.01] flex-grow overflow-hidden text-ellipsis whitespace-nowrap "
+						style={{
+							WebkitBoxFlex: 1,
+						}}
+					>
+						{title}
+					</span>
+				</span>
+			</div>
+		</Link>
+	);
+};
+
+export default UserContextItem;
