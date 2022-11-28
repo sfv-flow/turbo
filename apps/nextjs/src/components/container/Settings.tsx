d8cc81f2c84d012cf814a3287a6fec2036a27c9f
itsnotaka <itsnotaka@gmail.com>
2022-11-28 12:51:12 -0500

    [ci] added Setting page container

--- apps/nextjs/src/components/container/Settings.tsx
+++ apps/nextjs/src/components/container/Settings.tsx
@@ -2,10 +2,10 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
 import Loading from "../Loading";
-import SideBar from "../sideBar";
+import SettingSidebar from "../sideBar/Settings";
 import WorkspaceNotFound from "../WorkspaceNotFound";
 
-const MainContainer = ({ children }: { children: React.ReactNode }) => {
+const SettingsContainer = ({ children }: { children: React.ReactNode }) => {
 	const router = useRouter();
 	// with require being true the user will be directly pushed to the login page if they are not logged in
 	const { data: session } = useSession({
@@ -38,7 +38,7 @@ const MainContainer = ({ children }: { children: React.ReactNode }) => {
 			className="fixed flex h-full min-h-full w-full flex-row overflow-hidden pr-[env(safe-area-inset-right,0px)] pl-[env(safe-area-inset-left,0px)] text-[#eeeffc] [border-top:none] lg:static lg:z-auto"
 			style={{ WebkitBoxAlign: "stretch" }}
 		>
-			{session && workspaceData && <SideBar workspace={workspaceData} />}
+			{session && workspaceData && <SettingSidebar workspace={workspaceData} />}
 			<main
 				className="relative flex flex-initial flex-grow flex-col place-items-stretch overflow-auto text-black [scrollbar-gutter:auto]"
 				style={{ WebkitBoxAlign: "stretch", WebkitBoxFlex: 1 }}
@@ -49,4 +49,4 @@ const MainContainer = ({ children }: { children: React.ReactNode }) => {
 	);
 };
 
-export default MainContainer;
+export default SettingsContainer;
