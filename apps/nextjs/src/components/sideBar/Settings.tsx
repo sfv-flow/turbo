af09dcbdb7105fe46a99c1f20dd8b069343526a1
itsnotaka <itsnotaka@gmail.com>
2022-11-30 14:43:23 -0500

    [fix] invalid import

--- apps/nextjs/src/components/sideBar/Settings.tsx
+++ apps/nextjs/src/components/sideBar/Settings.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { trpc } from "../../utils/trpc";
 import UserContextMenu from "../menu/UserContext";
 import WorkspaceContextMenu from "../menu/WorkspaceContext";
-import SearchBar from "./SearchBar";
+import CommandBar from "../commandBar";
 import AccountSettingsItems from "./AccountSettingsItems";
 
 const SettingSidebar = ({
